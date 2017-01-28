e78352b7bdb96575ead492b1d4a88d08a62e50f3
stygmah <stygmah@gmail.com>
2017-01-28 19:55:01 +0100

    2

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const socketIO = require('socket.io');
+const http = require('http');
 const path = require('path');
 const express = require('express');
 
@@ -5,12 +7,16 @@ const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+	console.log(socket.connected);
+});
 
 
-
-app.listen(port,()=>{
+server.listen(port,()=>{
 	console.log('server running on port '+port);
 });
